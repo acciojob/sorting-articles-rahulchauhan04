cedb27ffde8cdb74d3d17a0360598b1f58ee0c76
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-05-16 23:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 //your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = [
+    'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 
+    'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 
+    'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 
+    'An Old Dog'
+];
 
 function strip(article) {
-	return article.replace(/^(a |an |the)/i, '').trim();
+    return article.replace(/^(a |an |the )/i, '').trim();
 }
 
-const sortedBands = bands.sort((a,b) => strip(a).localeCompare(strip(b)));
+const sortedBands = bands.sort((a, b) => strip(a).localeCompare(strip(b)));
 
-const ul = document.getElementById('band');
+const ul = document.getElementById('bands');
 
 sortedBands.forEach((band) => {
-	const li = document.createElement('li');
-	li.textContent = band;
-	ul.appendChild(li);
-})
+    const li = document.createElement('li');
+    li.textContent = band;
+    ul.appendChild(li);
+});
