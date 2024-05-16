34b21eaac6438b40cd200483ccf8f4f8b43735c8
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-05-16 23:31:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
 function strip(article) {
-	return article.replace(/^(a |an |the)i, '').trim();
+	return article.replace(/^(a |an |the)/i, '').trim();
 }
 
 const sortedBands = bands.sort((a,b) => strip(a).localeCompare(strip(b)));
 
-const ul = documentl.getElementById('band');
+const ul = document.getElementById('band');
 
 sortedBands.forEach((band) => {
 	const li = document.createElement('li');
